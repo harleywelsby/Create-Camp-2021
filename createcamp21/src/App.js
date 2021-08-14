d58f41e754f02258cc302a9be6aedfdb3598a76b
h9rl <harleywelsby9@gmail.com>
2021-08-14 15:47:37 +1200

    CSV Parsing & Sorting

--- createcamp21/src/App.js
+++ createcamp21/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BankStatement from './Statement.js';
 import ModelProjections from './ModelProjections';
+import InputFile from './InputFile.js';
 
 function App() {
   return (
     <div className="App">
       <BankStatement/>
       <ModelProjections/>
+      <InputFile/>
     </div>
   );
 }
