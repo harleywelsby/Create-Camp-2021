189b7ebebfa3990cfa5d275761f1600a22d72ea0
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-08-14 14:29:25 +1200

    Moved graph into graphs area

--- createcamp21/src/App.js
+++ createcamp21/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import MoneyLine from "./LineChart.js"
-
-const data = [{name: 'January', spendings: 400},{name: 'February', spendings: 300}, {name:'February',spendings:500}];
 import BankStatement from './Statement.js';
 import ModelProjections from './ModelProjections';
 
 function App() {
   return (
     <div className="App">
-      <MoneyLine data={data}/>
       <BankStatement/>
       <ModelProjections/>
     </div>
