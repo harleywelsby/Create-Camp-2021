8e0557b289ddd61663dc62e1a98a1f207099773f
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-08-14 14:02:10 +1200

    Basic account display with show/calculate graphs

--- createcamp21/src/App.js
+++ createcamp21/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BankStatement from './Statement.js';
+import ModelProjections from './ModelProjections';
 
 function App() {
   return (
     <div className="App">
       <BankStatement/>
+      <ModelProjections/>
     </div>
   );
 }
--- createcamp21/src/Statement.js
+++ createcamp21/src/Statement.js
@@ -1,7 +1,28 @@
 function BankStatement(){
-    return <div class="card-div">
-        
-        </div>;
+    return (<div className="card-div">
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Date and Time
+                        </th>
+                        <th>
+                            Account Number
+                        </th>
+                        <th>
+                            Amount
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>AawdawdAAAAAA</td> 
+                        <td>AAAAAAA</td> 
+                        <td>AAAAawdawdwadAAA</td> 
+                    </tr>
+                </tbody>
+            </table>
+        </div>);
 }
 
 export default BankStatement;
