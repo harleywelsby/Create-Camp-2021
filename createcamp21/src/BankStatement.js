b1c34a7525b85796f7ec934708e81f6df6bc95d5
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-08-14 16:24:18 +1200

    Displaying bank account details

--- createcamp21/src/BankStatement.js
+++ createcamp21/src/BankStatement.js
@@ -17,12 +17,7 @@ function BankStatement(){
                         </th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>AawdawdAAAAAA</td> 
-                        <td>AAAAAAA</td> 
-                        <td>AAAAawdawdwadAAA</td> 
-                    </tr>
+                <tbody id="table-body">
                 </tbody>
             </table>
         </div>);
--- createcamp21/src/InputFile.js
+++ createcamp21/src/InputFile.js
@@ -20,12 +20,14 @@ function InputFile() {
         date: data[i].data[11],
         desc: data[i].data[6],
         firstName: data[i].data[9],
-        extraction: data[i].data[16]
+        extraction: data[i].data[16],
+        accountnumber: data[i].data[3]
       }
       transactions.push(transaction);
     }
-
     for(let i=0; i<transactions.length; i++){
+      var transaction = transactions[i];
+      StatementRow(transaction.date, transaction.accountnumber,transaction.amount, null);
       var graphNode = {
         name: transactions[i].date,
         spendings: transactions[i].balance
@@ -34,6 +36,25 @@ function InputFile() {
     }
 
   };
+
+  function StatementRow(date,account,amount,category) {
+    var table_body = document.getElementById("table-body");
+    var tablerow = document.createElement("tr");
+    var tabledate = document.createElement("td");
+    tabledate.innerHTML = date;
+    var tableaccount = document.createElement("td");
+    tableaccount.innerHTML = account;
+    var tableamount = document.createElement("td");
+    tableamount.innerHTML = amount;
+    var tablecategory = document.createElement("td");
+    tablecategory.innerHTML = category;
+    tablerow.append(tabledate);
+    tablerow.append(tableaccount);
+    tablerow.append(tableamount);
+    tablerow.append(tablecategory);
+    table_body.append(tablerow);
+    
+}
 
   return (
     <div className="fileSystem">
